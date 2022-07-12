672736b166df61f80e03a639a2d727912a4a60aa
Isaribi <ndurska638@gmail.com>
2022-07-12 13:24:24 +0200

    First commit

--- app/src/main/java/com/ndurska/coco3/MainActivity.java
+++ app/src/main/java/com/ndurska/coco3/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private int activeClientPosition;
 
 
-
+/////////////AAAAAAAAAAAAAA
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
